0950ca386c5b997a2eed33a2f3eb1b4c8eb25358
Jeff So <jeffso9820@gmail.com>
2020-10-14 17:51:48 -0400

    "added ExerciseTests"

---
+++ src/test/model/ExerciseTest.java
@@ -0,0 +1,44 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExerciseTest {
+    private Exercise testExercise;
+
+    @BeforeEach
+    public void runBefore() {
+        testExercise = new Exercise("Bench Press", ExerciseType.CHEST);
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals(testExercise.getName(), "Bench Press");
+        assertEquals(testExercise.getType(), ExerciseType.CHEST);
+        assertEquals(testExercise.getWeight(), 0);
+        assertEquals(testExercise.getSets(), 3);
+        assertEquals(testExercise.getReps(),5);
+    }
+
+    @Test
+    public void setWeightTest(){
+        assertEquals(testExercise.getWeight(), 0);
+        testExercise.setWeight(225);
+        assertEquals(testExercise.getWeight(), 225);
+    }
+
+    @Test
+    public void setSetsTest(){
+        assertEquals(testExercise.getSets(), 3);
+        testExercise.setSets(4);
+        assertEquals(testExercise.getSets(), 4);
+    }
+
+    @Test
+    public void setRepsTest(){
+        assertEquals(testExercise.getReps(), 5);
+        testExercise.setReps(12);
+        assertEquals(testExercise.getReps(), 12);
+    }
+}
